460faf0fcf5dbdc1c1ebf1df197d165e437f77f6
BrechtMaesPXL <92795017+BrechtMaesPXL@users.noreply.github.com>
2024-05-07 01:59:44 +0200

    add the pwa in a nieuwe app

---
+++ frontend/src/components/FooterComponent.vue
@@ -0,0 +1,72 @@
+<script>
+import IconHome from "@/components/icons/IconHome.vue";
+import IconNotification from "@/components/icons/IconNotification.vue";
+import IconAccount from "@/components/icons/IconAccount.vue";
+import {RouterView} from "vue-router";
+export default {
+  name: "FooterComponent",
+  components: {RouterView, IconAccount, IconNotification, IconHome}
+}
+</script>
+
+<template>
+  <footer>
+    <div class="wrapper">
+      <nav>
+<!--        <RouterLink to="/">-->
+<!--          <IconHome />-->
+<!--        </RouterLink>-->
+<!--        <RouterLink to="/notifications">-->
+<!--          <IconNotification />-->
+<!--        </RouterLink>-->
+<!--        <RouterLink to="/account">-->
+<!--          <IconAccount />-->
+<!--        </RouterLink>-->
+      </nav>
+    </div>
+  </footer>
+  <div class="router"><Suspense><RouterView /></Suspense></div>
+</template>
+
+<style scoped>
+footer {
+  width: 100%;
+  position: fixed;
+  bottom: 0;
+  z-index: 1000;
+}
+
+.router {
+  padding-top: 75px;
+  padding-bottom: 65px;
+}
+
+nav {
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  width: 100%;
+  text-align: center;
+  padding: 1rem 0;
+  justify-content: center;
+  background: #f5ecbd;
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1.5rem;
+}
+
+nav a svg {
+  width: 2.5rem;
+  height: 2.5rem;
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
